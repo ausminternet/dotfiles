3d2059878e97ee55617f7830e6e3a60bd8f440ed
Jan Hofmann <jan.hofmann@dexite.de>
2017-02-05 09:38:54 +0100

    add some new brew packages and make small fixes

--- install/brew.sh
+++ install/brew.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 BREW_PACKAGES=(
-	tree
-	git
-	git-fow-avh
-	hugo
-	youtube-dl
-	unrar
-	node@6
-	wget
-	bash-completion
+  tree
+  git
+  git-flow-avh
+  hugo
+  youtube-dl
+  unrar
+  node@6
+  wget
+  bash-completion
 )
 
 BREW_CASK_PACKAGES=(
   1password
   dropbox
-  itsycal
   spotify
   whatsapp
   cyberduck
@@ -27,29 +26,37 @@ BREW_CASK_PACKAGES=(
   prepros
   sourcetree
   atom
+  trasmission
+  flux
+  qlmarkdown
+  quicklook-json
+  caffeine
+  appcleaner
 )
 
 # Check if Homebrew is installed and install if not
+BREW_INSTALLED="false"
+
 BREW=`which brew`
 
 if [[ -n "$BREW" ]]; then
-	echo "Found Homebrew."
+  echo "Found Homebrew."
+  BREW_INSTALLED="true"
 else
-	# Ask first
-	echo -n "Install Homebrew [Y/n]? "
-	read INSTALL_BREW
-	echo ""
+  # Ask first
+  echo -n "Install Homebrew [Y/n]? "
+  read INSTALL_BREW
+  echo ""
 
-	if [[ "$INSTALL_BREW" = "N" || "$INSTALL_BREW" = "n" ]]; then
-		echo "Skipping Homebrew..."
-    BREW_INSTALLED = false
-	else
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    BREW_INSTALLED = true
-	fi
+  if [[ "$INSTALL_BREW" = "N" || "$INSTALL_BREW" = "n" ]]; then
+    echo "Skipping Homebrew..."
+  else
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    BREW_INSTALLED="true"
+  fi
 fi
 
-if [ "$BREW_INSTALLED" = true ] ; then
+if [ "$BREW_INSTALLED" = "true" ] ; then
   # Install base packages
   printf 'Base packages include: %s\n' "${BREW_PACKAGES[*]}"
   echo -n "Install base packages via brew [Y/n]? "
@@ -59,10 +66,7 @@ if [ "$BREW_INSTALLED" = true ] ; then
     echo "Skipping base packages..."
   else
     echo "Installing base packages..."
-
-    for PACKAGE in $BREW_PACKAGES; do
-      /usr/local/bin/brew install $PACKAGE
-    done
+    /usr/local/bin/brew install ${BREW_PACKAGES[@]}
   fi
 
   # Install brew cask packages
@@ -73,11 +77,9 @@ if [ "$BREW_INSTALLED" = true ] ; then
   if [[ "$INSTALL_CASK" = "N" || "$INSTALL_CASK" = "n" ]]; then
     echo "Skipping brew cask packages..."
   else
-		echo "activate casks..."
-		brew tap caskroom/cask
+    echo "activate casks..."
+    brew tap caskroom/cask
     echo "Installing brew cask packages..."
-    for PACKAGE in $BREW_CASK_PACKAGES; do
-      /usr/local/bin/brew cask install $PACKAGE
-    done
+    /usr/local/bin/brew cask install ${BREW_CASK_PACKAGES[@]}
   fi
 fi
